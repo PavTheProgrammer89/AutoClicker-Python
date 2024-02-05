0f390983faa70390daf710b4f778e704e61b4d98
PavTheProgrammer <paveliv2015@gmail.com>
2024-02-05 17:46:26 +0100

    Ceated Repository

---
+++ AutoClicker.py
@@ -0,0 +1,21 @@
+import pyautogui
+import keyboard
+import time
+
+def auto_click(num_clicks, interval):
+    print("Start AutoClicker (F6"
+          "End AutoClicker HOLD F7)")
+    while True:
+        keyboard.wait("F6")
+        for _ in range(num_clicks):
+            if keyboard.is_pressed("F7"):
+                return
+            print("AutoClicer: Printed Succesfully!")
+            pyautogui.click()
+            time.sleep(interval)
+
+
+    print("AutoClicker: Ended Clicking Succesfully!")
+
+num_clicks = 50
+interval = 0.1
