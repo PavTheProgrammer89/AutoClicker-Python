1bbd5c419fca3e81037a60ee5619ad4885ae4da5
PavTheProgrammer <paveliv2015@gmail.com>
2024-02-05 18:05:30 +0100

    FixedCrush

--- AutoClicker.py
+++ AutoClicker.py
@@ -3,14 +3,14 @@ import keyboard
 import time
 
 def auto_click(num_clicks, interval):
-    print("Start AutoClicker (F6"
+    print("Start AutoClicker (F6 "
           "End AutoClicker HOLD F7)")
     while True:
         keyboard.wait("F6")
         for _ in range(num_clicks):
             if keyboard.is_pressed("F7"):
                 return
-            print("AutoClicer: Printed Succesfully!")
+            print("AutoClicer: Clicked Succesfully!")
             pyautogui.click()
             time.sleep(interval)
 
@@ -19,3 +19,4 @@ def auto_click(num_clicks, interval):
 
 num_clicks = 50
 interval = 0.1
+auto_click(num_clicks, interval)
